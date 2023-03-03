43acc1596ce3cc164fa2d7ffccfd46ac2de8caaf
Palak <Palakgoyal0304@gmail.com>
2023-03-03 11:56:12 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'light-peach': '#fffbe2'
+      }
+    },
+  },
+  plugins: [],
+}
